e17aea7e010e11f418d7cd39f4cdc5268a9e8e3b
iray3 <kasireddy.sriharsha@gmail.com>
2025-01-20 22:45:29 +0530

    dd

--- 03-resources/data.tf
+++ 03-resources/data.tf
@@ -5,5 +5,5 @@ data "azurerm_resource_group" "example" {
 data "azurerm_subnet" "example" {
   name                 = "default"
   virtual_network_name = "workshop-vnet"
-  resource_group_name  = "data.azurerm_resource_group.example.name"
+  resource_group_name  = data.azurerm_resource_group.example.name
 }
