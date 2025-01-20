598d981f8a7d53406b059caca95ed07c10d2510c
iray3 <kasireddy.sriharsha@gmail.com>
2025-01-20 21:11:20 +0530

    dd

--- 02-data-source/main.tf
+++ 02-data-source/main.tf
@@ -5,3 +5,10 @@ data "azurerm_resource_group" "example" {
 output "rg" {
   value = data.azurerm_resource_group.example
 }
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+
+  features {}
+
+}
