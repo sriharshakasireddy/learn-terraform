af63789aa31b8fc62e20d05d627b95a04a1efe93
iray3 <kasireddy.sriharsha@gmail.com>
2025-01-20 23:15:39 +0530

    dd

--- 01.sample/main.tf
+++ 01.sample/main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "example" {
   name     = "harsha"
   location = "West Europe"
 }
+
+provider "azurerm" {
+
+  features {}
+  #subscription_id = "a92e07d8-3cdd-4fda-bb98-99b2dddb739c"
+}
