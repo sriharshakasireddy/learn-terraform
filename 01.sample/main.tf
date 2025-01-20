9d8c0ba32cddb6b5301a469a4e8098c30497c87e
iray3 <kasireddy.sriharsha@gmail.com>
2025-01-20 23:17:26 +0530

    dd

--- 01.sample/main.tf
+++ 01.sample/main.tf
@@ -6,5 +6,5 @@ resource "azurerm_resource_group" "example" {
 provider "azurerm" {
 
   features {}
-  #subscription_id = "a92e07d8-3cdd-4fda-bb98-99b2dddb739c"
+  subscription_id = "a92e07d8-3cdd-4fda-bb98-99b2dddb739c"
 }
